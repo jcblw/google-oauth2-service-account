c39b763848544167be3fcfe5378a052e09598a02
Jacob Lowe <jacoblowe2.0@gmail.com>
2014-02-08 10:04:34 -0800

    initial files

---
+++ index.js
@@ -0,0 +1,101 @@
+process.env.TZ = 'UTC';
+const request = require('request');
+const crypto = require('crypto');
+
+var auth = function(key, options, callback) {
+
+    var jwtHeaderB64,
+        iat,
+        exp,
+        jwtClaim,
+        jwtClaimB64,
+        signatureInput,
+        JWT,
+        signature;
+
+    if ( typeof options === 'function' ) {
+        callback = options;
+        options = {};
+    }
+
+    options.expires = options.expires || 3600; // in seconds
+    options.iss = options.iss || 'yourProjectID@developer.gserviceaccount.com';
+    options.scope = options.scope || "https://www.googleapis.com/auth/calendar";
+    options.aud = options.aud || "https://accounts.google.com/o/oauth2/token";
+
+    jwtHeader = {
+        alg: "RS256",
+        typ: "JWT"
+    };
+    jwtHeaderB64 = base64urlEncode(JSON.stringify(jwtHeader));
+    iat = Math.floor(new Date().getTime() / 1000);
+    exp = iat + options.expires;
+
+    jwtClaim = {
+        iss : options.iss,
+        scope : options.scope,
+        aud : options.aud
+    };
+    jwtClaim.exp = exp;
+    jwtClaim.iat = iat;
+    jwtClaimB64 = base64urlEncode(JSON.stringify(jwtClaim));
+    signatureInput = jwtHeaderB64 + '.' + jwtClaimB64;
+    signature = sign(signatureInput, key);
+
+
+    if ( typeof signature === 'object' ) {
+        return callback ( new Error(signature.error) );
+    }
+
+    JWT = signatureInput + '.' + signature;
+    request.post({
+        url: 'https://accounts.google.com/o/oauth2/token',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        form: {
+            grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+            assertion: JWT
+        }
+    }, function(err, res, body) {
+        var ret;
+        if (err) return callback( err );
+        if (res.statusCode !== 200) {
+            return callback(new Error("failed to retrieve an access token"), body);
+        }
+        try {
+            ret = JSON.parse(body)
+        } catch ( e ) {
+            return callback( e, body );
+        }
+
+        callback(err, ret.access_token);
+    });
+
+}
+
+function sign(inStr, key) {
+    var verifier,
+        sig;
+
+    if(key.length==0) return { error : "Invalid key form" };
+    sig = crypto.createSign('RSA-SHA256').update(inStr).sign(key, 'base64');
+    //verification
+    verifier = crypto.createVerify("RSA-SHA256");
+    verifier.update(inStr);
+    if(!verifier.verify(key, sig, 'base64')){        
+        return { error : "Signature NOT Verified" }
+    }
+
+    return base64urlEscape(sig);
+}
+
+function base64urlEncode(str) {
+    return base64urlEscape(new Buffer(str).toString('base64'));
+}
+
+function base64urlEscape(str) {
+    return str.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+}
+
+module.exports.auth = auth;
---
+++ test.js
@@ -0,0 +1,35 @@
+require('dotenv').load()
+
+var account = require('./index');
+var request = require('request');
+var key = process.env.KEY;
+
+const calRoot = "https://www.googleapis.com/calendar/v3";
+const calId = "04vr7pj9ccnnkudbr7lust24h4@group.calendar.google.com";
+const cal = "usa__en@holiday.calendar.google.com";
+
+account.auth( decodeURIComponent( key ), {
+        iss : process.env.ISS
+    }, function( err, accessToken ){
+
+    if ( err ) throw( err );
+
+    var token = "?access_token="+accessToken;
+
+    request.get({
+        url: calRoot + "/users/me/calendarList" + token,
+        json:true
+    }, function(err, res, body){
+
+        for(var c=0; c<body.items.length; c++){
+            console.log(body.items[c]);
+
+            request.get({
+                url:calRoot+"/calendars/"+body.items[c].id+"/events"+token,
+                json:true
+                }, function(err, res, cal){
+                    console.log(cal);
+            });
+        }
+    })
+})
